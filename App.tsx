2935da40690cb8f0775c8d7095a7b8301a9427d1
jordon0802 <jordon.chang.jj@gmail.com>
2024-05-15 23:10:15 +0800

    homepage-ish

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'turquoise',
+        flex: 1,
+        justifyContent: 'center',
+        padding: 5,
+    },
+    brand: {
+        color: 'darkblue',
+        fontSize: 60,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    textInput: {
+        color: 'black',
+        textAlign: 'center',
+    }
+});
+
 
 const App = () => {
     return (
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 50 }}>Hello, world!</Text>
+        <View style={styles.background}>
+            <Text style={styles.brand}>Study Sphere</Text>
+            <TextInput style={styles.textInput}>Enter you name</TextInput>
         </View>
     );
 };
